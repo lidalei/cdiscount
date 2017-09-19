dd43ed02c01b0e048d6b966265eb6ccf9331c605
Dalei Li <lidalei151781@gmail.com>
2017-09-19 09:58:51 +0200

    Set category_id as -1 for the test dataset.

--- read_data.py
+++ read_data.py
@@ -80,6 +80,10 @@ class BsonReader(object):
         ratio = ratios[0] / sum(ratios)
         with TFRecordWriter(filenames[0]) as tfwriter1, TFRecordWriter(filenames[1]) as tfwriter2:
             for c, d in enumerate(self.data):
+                # Test dataset.
+                if 'category_id' not in d:
+                    d['category_id'] = -1
+
                 feature = {
                     '_id': self._int64_feature(d['_id']),
                     'category_id': self._int64_feature(category_id_mapping[d['category_id']])
