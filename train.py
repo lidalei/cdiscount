2a442d663fb1597e2acc53c65b15fdbb9f8f422f
Dalei Li <lidalei151781@gmail.com>
2017-10-16 13:14:19 +0200

    Fixed reshape size argument.

--- train.py
+++ train.py
@@ -202,7 +202,7 @@ def main(unused_argv):
 
     # TODO, Change Me!
     tr_data_fn = tr_data_conv_fn
-    tr_data_paras = {'reshape': True, 'size': 1536}
+    tr_data_paras = {'reshape': True, 'size': 2048}
 
     train_data_pipeline = DataPipeline(reader=reader,
                                        data_pattern=FLAGS.train_data_pattern,
